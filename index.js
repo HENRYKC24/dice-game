312cce4414dbc7f294051af7dd382ae2e35376d7
HENRY-KC <henrykc24@yahoo.com>
2020-05-05 13:39:00 +0100

    feature: result works

--- index.js
+++ index.js
@@ -3,17 +3,35 @@ var img2 = document.querySelector('#img2');
 var btn1 = document.querySelector('#btn1');
 var btn2 = document.querySelector('#btn2');
 var doubleBtn = document.querySelector('#doubleBtn');
+var player1Score = 0;
+var player2Score = 0;
 function randNum() {
   return Math.floor(Math.random() * 6) + 1;
 }
 function rollDice1() {
-  var image = "images/dice" + randNum() + ".png";
+  player1Score = randNum();
+  var image = "images/dice" + player1Score + ".png";
   img1.setAttribute('src', image);
+  btn1.setAttribute('disabled', 'disabled');
+  btn2.removeAttribute('disabled');
 }
 function rollDice2() {
-  var image = "images/dice" + randNum() + ".png";
+  player2Score = randNum();
+  var image = "images/dice" + player2Score + ".png";
   img2.setAttribute('src', image);
-}
+  btn2.setAttribute('disabled', 'disabled');
+  btn1.removeAttribute('disabled');
+  var h1 = document.querySelector('h1');
+  if ( player1Score === player2Score ) {
+    h1.textContent = 'That\'s a tie!';
+  }
+  else if ( player1Score > player2Score ) {
+    h1.textContent = 'Player 1 Wins!';
+  }
+  else {
+    h1.textContent = 'Player 2 Wins!';
+  }
+};
 rollDice1();
 rollDice2();
 btn1.setAttribute('onclick', 'rollDice1()');
