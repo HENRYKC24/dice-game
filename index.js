5e53c3ae94c96cf8d1207c75ad025c079467b552
HENRY-KC <henrykc24@yahoo.com>
2020-05-05 17:30:13 +0100

    fix: change HTML file to index.html

--- index.js
+++ index.js
@@ -5,25 +5,48 @@ var btn2 = document.querySelector('#btn2');
 var doubleBtn = document.querySelector('#doubleBtn');
 var player1Score = 0;
 var player2Score = 0;
+var h1 = document.querySelector('h1');
 function randNum() {
   return Math.floor(Math.random() * 6) + 1;
 }
+// element.addEventListener("webkitAnimationEnd", callfunction,false);
+// element.addEventListener("animationend", callfunction,false);
+// element.addEventListener("oanimationend", callfunction,false);
+function animate1() {
+  img1.setAttribute('src', 'images/dice5.png');
+  img1.classList.add('rotate');
+}
+img1.addEventListener('webkitAnimationEnd', rollDice1);
+img1.addEventListener('animationend', rollDice1);
+img1.addEventListener('oanimationend', rollDice1);
+
+function animate2() {
+  img2.setAttribute('src', 'images/dice5.png');
+  img2.classList.add('rotate');
+}
+img2.addEventListener('webkitAnimationEnd', rollDice2);
+img2.addEventListener('animationend', rollDice2);
+img2.addEventListener('oanimationend', rollDice2);
+
 function rollDice1() {
+  img2.classList.remove('rotate');
   player1Score = randNum();
   var image = "images/dice" + player1Score + ".png";
   img1.setAttribute('src', image);
   btn1.setAttribute('disabled', 'disabled');
   btn2.removeAttribute('disabled');
+  h1.innerHTML = 'Waiting for Player 2...';
 }
 function rollDice2() {
+  img1.classList.remove('rotate');
+  // img2.classList.add('rotate');
   player2Score = randNum();
   var image = "images/dice" + player2Score + ".png";
   img2.setAttribute('src', image);
   btn2.setAttribute('disabled', 'disabled');
   btn1.removeAttribute('disabled');
-  var h1 = document.querySelector('h1');
   if ( player1Score === player2Score ) {
-    h1.innerHTML = 'That\'s a tie!';
+    h1.innerHTML = 'That\'s a Draw!';
   }
   else if ( player1Score > player2Score ) {
     h1.innerHTML = '<i class="fas fa-flag-checkered"></i> Player 1 Wins!';
@@ -32,8 +55,11 @@ function rollDice2() {
     h1.innerHTML = 'Player 2 Wins! <i class="fas fa-flag-checkered"></i>';
   }
 };
-rollDice1();
-rollDice2();
-btn1.setAttribute('onclick', 'rollDice1()');
-btn2.setAttribute('onclick', 'rollDice2()');
-doubleBtn.setAttribute('onclick', 'rollDice1();rollDice2();');
+animate1();
+animate2();
+btn1.setAttribute('onclick', 'animate1()');
+btn2.setAttribute('onclick', 'animate2()');
+
+doubleBtn.addEventListener('click', function(){
+  document.location.reload();
+});
